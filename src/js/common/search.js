f2ba04293e9c3220d0640ff852c18182d985f1ab
rentaire <64122338+rentaire@users.noreply.github.com>
2021-07-07 08:18:55 +1000

    Изменен поиск

--- src/js/common/search.js
+++ src/js/common/search.js
@@ -7,31 +7,10 @@ import { sendRequest } from './../modules/sendRequest'
 
 let input = document.getElementById('text-to-find');
 
-// input.addEventListener("keydown", function (event) {
-//     if (event.key === "Enter") {
-//         event.preventDefault();
-
-//         var q = input.value
-//         var parsedUrl = {
-//             endpoint: 'top-headlines',
-//             parameters: 'q=' + q,
-//             page: '1'
-//         }
-
-//         //history.pushState(null, null, '?top-headlines_q=' + q)
-
-//         console.log(createUrl(parsedUrl))
-//         updateArticles(createUrl(parsedUrl))
-//         // updateArticles(createUrl(parseUrl(window.location.search)))
-//     }
-// });
-
 input.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
-
         
-
         var q = input.value
         var parsedUr = {
             endpoint: 'top-headlines',
@@ -39,28 +18,7 @@ input.addEventListener("keydown", function (event) {
             page: '1'
         }
 
-        document.location.href = createUrl(parsedUr)
-
-        sendRequest(createUrl(parsedUr)).then(function (response) {
-            return response.json()
-                .then((data) => {
-                    deleteArticles(data.articles.length)
-                    showResult(data.articles)
-        
-                    // let max_page = Math.ceil(data.totalResults / page_size)
-                    // last_page.textContent = max_page
-                    // last_page.setAttribute('data-page', max_page)
-        
-                    // createPaginationList(max_page)
-                })
-        })
-
+        document.location.href = 'articles.html?' + parsedUr.endpoint + '_' + parsedUr.parameters
     }
 });
 
-var deleteArticles = (length) => {
-    for (let i = page_size - 1; i >= length; i--) {
-        var articles_block = document.querySelector('.articles__block')
-        articles_block.removeChild(document.querySelectorAll('.article')[i])
-    }
-}
--- src/js/modules/showResult.js
+++ src/js/modules/showResult.js
@@ -3,7 +3,6 @@ import { getDate } from './dateToRu.js';
 export function showResult(data) {
     let articles = Array.from(document.querySelectorAll('.article'))
     var article_counter = 0
-    console.log(data)
     articles.forEach(article => {
         article.querySelector('.article__img').src = data[article_counter].urlToImage
         article.querySelector('.article__title').textContent = data[article_counter].title
